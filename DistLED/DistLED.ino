f3a4463d381410c25d9202b4a19c89918cc4d101
jy97 <jun.yoyoyoshida@gmail.com>
2013-12-14 18:01:50 +0100

    added new sample sketch for arduino & processing

--- DistLED/DistLED.ino
+++ DistLED/DistLED.ino
@@ -1,46 +1,60 @@
----------------------------------------------------------------------
-#define USS_T_PIN 7      // 送信側センサー接続のピン番号(デジタル接続)
-#define USS_R_PIN 0      // 受信側センサー接続のピン番号(アナログ接続)
+/*
+ HC-SR04 Ping distance sensor:
+ VCC to arduino 5v 
+ GND to arduino GND
+ Echo to Arduino pin 7 
+ Trig to Arduino pin 8
+ 
+ This sketch originates from Virtualmix: http://goo.gl/kJ8Gl
+ Has been modified by Winkle ink here: http://winkleink.blogspot.com.au/2012/05/arduino-hc-sr04-ultrasonic-distance.html
+ And modified further by ScottC here: http://arduinobasics.blogspot.com/
+ on 10 Nov 2012.
+ */
 
 
-//  電源起動時とリセットの時だけのみ処理される関数(初期化と設定処理)
+#define echoPin 7 // Echo Pin
+#define trigPin 8 // Trigger Pin
+#define LEDPin 13 // Onboard LED
+
+int maximumRange = 200; // Maximum range needed
+int minimumRange = 0; // Minimum range needed
+long duration, distance; // Duration used to calculate distance
+
 void setup() {
-  Serial.begin(9600);    // 9600bpsでシリアル通信のポートを開きます
+ Serial.begin (9600);
+ pinMode(trigPin, OUTPUT);
+ pinMode(echoPin, INPUT);
+ pinMode(LEDPin, OUTPUT); // Use LED indicator (if required)
 }
-//  繰り返し実行される処理の関数(メインの処理)
+
 void loop() {
-     int ans ;
+/* The following trigPin/echoPin cycle is used to determine the
+ distance of the nearest object by bouncing soundwaves off of it. */ 
+ digitalWrite(trigPin, LOW); 
+ delayMicroseconds(2); 
 
-     ans = UsonicMeasurRead(USS_T_PIN,USS_R_PIN,20,30) ;
-     Serial.print(ans) ;                // 表示を行う
-     Serial.println("mm") ;
-     delay(500) ;                       // 500ms後に繰り返す
+ digitalWrite(trigPin, HIGH);
+ delayMicroseconds(10); 
+ 
+ digitalWrite(trigPin, LOW);
+ duration = pulseIn(echoPin, HIGH);
+ 
+ //Calculate the distance (in cm) based on the speed of sound.
+ distance = duration/58.2;
+ 
+ if (distance >= maximumRange || distance <= minimumRange){
+ /* Send a negative number to computer and Turn LED ON 
+ to indicate "out of range" */
+ Serial.println("-1");
+ digitalWrite(LEDPin, HIGH); 
+ }
+ else {
+ /* Send the distance to the computer using Serial protocol, and
+ turn LED OFF to indicate successful reading. */
+ Serial.println(distance);
+ digitalWrite(LEDPin, LOW); 
+ }
+ 
+ //Delay 50ms before next reading.
+ delay(50);
 }
-// 超音波センサーから距離を得る処理
-// txpin=送信接続ピン  rxpin=受信接続ピン  temp=周辺温度  correction=距離補正
-// 距離をｍｍで返す(検出できない場合は０を返す)
-int UsonicMeasurRead(int txpin,int rxpin,int temp,int correction)
-{
-     unsigned long t , t2 ;
-     int ans , val ;
-
-     ans = 0 ;
-     tone(txpin,40000) ;                // 送信センサーに40KHz信号を200us出力
-     delayMicroseconds(200) ;
-     noTone(txpin) ;                    // 40KHz信号停止
-     t = micros() ;                     // 現在の時間を記憶する
-     while(1) {
-          val = analogRead(rxpin) ;     // 受信センサーの値を読む
-          t2 = micros() - t ;           // 時間をカウントする
-          if (val >= 410) break ;       // 受信値の電圧が2V以上なら反射信号とする
-          if (t2 >= 30000) break ;      // 反射信号が返って来なかった場合の処理
-     }
-     if (t2 < 30000) {
-          t = 331500 + (600 * temp) ;   // 音波の伝搬する速度を求める
-          t = (t * t2) / 1000000 ;      // 距離の計算
-          ans = t / 2 ;                 // 往復なので÷2
-          ans = ans + correction ;      // 距離の補正値を加える
-     }
-     return ans ;
-}
----------------------------------------------------------------------
